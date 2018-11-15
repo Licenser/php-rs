11393a19832c9e162f0856a4d3ba0e0d9dddfb9f
Heinz N. Gies <heinz@licenser.net>
2018-11-15 21:48:50 +0100

    Linux (#3)
    build on linux and os x

--- php-sys/build.rs
+++ php-sys/build.rs
@@ -3,6 +3,7 @@ extern crate cc;
 extern crate num_cpus;
 
 use bindgen::callbacks::{MacroParsingBehavior, ParseCallbacks};
+use bindgen::Builder;
 use std::collections::HashSet;
 use std::env;
 use std::io::Write;
@@ -51,6 +52,7 @@ fn exists(path: &str) -> bool {
     Path::new(target(path).as_str()).exists()
 }
 
+/// This is needed to prevent bindgen to create multiple definitions of the same macro and fail
 #[derive(Debug)]
 struct MacroCallback {
     macros: Arc<RwLock<HashSet<String>>>,
@@ -60,25 +62,33 @@ impl ParseCallbacks for MacroCallback {
     fn will_parse_macro(&self, name: &str) -> MacroParsingBehavior {
         self.macros.write().unwrap().insert(name.into());
 
-        if name == "FP_NAN" {
-            return MacroParsingBehavior::Ignore;
+        match name {
+            "FP_NAN" | "FP_INFINITE" | "FP_ZERO" | "FP_SUBNORMAL" | "FP_NORMAL" => {
+                MacroParsingBehavior::Ignore
+            }
+            _ => MacroParsingBehavior::Default,
         }
-
-        MacroParsingBehavior::Default
     }
 }
 
 fn main() {
     let cpus = format!("{}", num_cpus::get());
+    #[cfg(all(target_os = "linux"))]
+    let default_link_static = false;
+    #[cfg(all(target_os = "macos"))]
     let default_link_static = true;
     let php_version = option_env!("PHP_VERSION").unwrap_or(PHP_VERSION);
     let macros = Arc::new(RwLock::new(HashSet::new()));
 
     println!("cargo:rerun-if-env-changed=PHP_VERSION");
+    println!("cargo:rerun-if-env-changed=PHP_LINK_STATIC");
 
+    let link_dynamic = env::var_os("PHP_LINK_DYNAMIC")
+        .map(|_| true)
+        .unwrap_or(false);
     let link_static = env::var_os("PHP_LINK_STATIC")
         .map(|_| true)
-        .unwrap_or(default_link_static);
+        .unwrap_or(default_link_static && !link_dynamic);
 
     if !exists("php-src/LICENSE") {
         println_stderr!("Setting up PHP {}", php_version);
@@ -93,7 +103,7 @@ fn main() {
         );
         run_command_or_fail(
             target("php-src"),
-            "echo",
+            "sed",
             &[
                 "-e",
                 "s/void zend_signal_startup/ZEND_API void zend_signal_startup/g",
@@ -104,23 +114,46 @@ fn main() {
         );
         run_command_or_fail(target("php-src"), "./genfiles", &[]);
         run_command_or_fail(target("php-src"), "./buildconf", &["--force"]);
-        run_command_or_fail(
-            target("php-src"),
-            "./configure",
-            &[
-                "--enable-debug",
-                "--enable-embed=static",
-                "--without-iconv",
-                "--disable-libxml",
-                "--disable-dom",
-                "--disable-xml",
-                "--enable-maintainer-zts",
-                "--disable-simplexml",
-                "--disable-xmlwriter",
-                "--disable-xmlreader",
-                "--without-pear",
-            ],
-        );
+
+        let embed_type = if link_static { "static" } else { "shared" };
+
+        #[cfg(all(target_os = "linux"))]
+        let config = &[
+            "--enable-debug",
+            &format!("--enable-embed={}", embed_type),
+            "--disable-cli",
+            "--disable-cgi",
+            "--enable-maintainer-zts",
+            // "--without-iconv",
+            "--disable-libxml",
+            "--disable-dom",
+            "--disable-xml",
+            "--disable-simplexml",
+            "--disable-xmlwriter",
+            "--disable-xmlreader",
+            // "--without-pear",
+            // "--with-libdir=lib64",
+            // "--with-pic",
+        ];
+        #[cfg(all(target_os = "macos"))]
+        let config = &[
+            "--enable-debug",
+            &format!("--enable-embed={}", embed_type),
+            "--disable-cli",
+            "--disable-cgi",
+            "--enable-maintainer-zts",
+            "--without-iconv",
+            "--disable-libxml",
+            "--disable-dom",
+            "--disable-xml",
+            "--disable-simplexml",
+            "--disable-xmlwriter",
+            "--disable-xmlreader",
+            // "--without-pear",
+            // "--with-libdir=lib64",
+            // "--with-pic",
+        ];
+        run_command_or_fail(target("php-src"), "./configure", config);
         run_command_or_fail(target("php-src"), "make", &["-j", cpus.as_str()]);
     }
 
@@ -137,18 +170,39 @@ fn main() {
         .map(|d| format!("-I{}{}", include_dir, d))
         .collect::<Vec<String>>();
 
-    let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
+    let bindings = Builder::default()
+        .rustfmt_bindings(true)
         .clang_args(includes)
-        .blacklist_type("FP_NAN")
-        .blacklist_type("FP_INFINITE")
-        .blacklist_type("FP_ZERO")
-        .blacklist_type("FP_SUBNORMAL")
-        .blacklist_type("FP_NORMAL")
-        .blacklist_type("max_align_t")
+        .whitelist_function("_zend_file_handle__bindgen_ty_1")
+        .whitelist_function("php_execute_script")
+        .whitelist_function("php_module_startup")
+        .whitelist_function("php_request_shutdown")
+        .whitelist_function("php_request_startup")
+        .whitelist_function("phprpm_fopen")
+        .whitelist_function("sapi_send_headers")
+        .whitelist_function("sapi_startup")
+        .whitelist_function("sg_request_info")
+        .whitelist_function("sg_sapi_headers")
+        .whitelist_function("sg_server_context")
+        .whitelist_function("sg_server_context")
+        .whitelist_function("sg_set_server_context")
+        .whitelist_function("sg_set_server_context")
+        .whitelist_function("ts_resource_ex")
+        .whitelist_function("tsrm_startup")
+        .whitelist_function("zend_error")
+        .whitelist_function("zend_signal_startup")
+        .whitelist_function("zend_tsrmls_cache_update")
+        .whitelist_var("SAPI_HEADER_SENT_SUCCESSFULLY")
+        .whitelist_type("sapi_headers_struc")
+        .whitelist_type("sapi_module_struc")
+        .whitelist_type("sapi_request_info")
+        .whitelist_type("ZEND_RESULT_CODE")
+        .whitelist_type("zval")
+        .whitelist_var("zend_stream_type_ZEND_HANDLE_FP")
         .parse_callbacks(Box::new(MacroCallback {
             macros: macros.clone(),
         })).derive_default(true)
+        .header("wrapper.h")
         .generate()
         .expect("Unable to generate bindings");
 
@@ -159,6 +213,8 @@ fn main() {
     cc::Build::new()
         .file("src/shim.c")
         .include(&include_dir)
+        .flag("-fPIC")
+        .flag("-m64")
         .include(&format!("{}/TSRM", include_dir))
         .include(&format!("{}/Zend", include_dir))
         .include(&format!("{}/main", include_dir))
